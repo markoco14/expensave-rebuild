f21e0287489c07c625005e0c6e5dbef225ef2587
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-23 22:59:24 +0800

    create track-purchase endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """ Main application file """
-
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -11,3 +11,13 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/track-purchase")
+def track_purchase(
+    request: Request,
+    items: Annotated[str, Form()],
+    price: Annotated[str, Form()],
+    currency: Annotated[str, Form()],
+    location: Annotated[str, Form()]
+    ):
+    return templates.TemplateResponse("track-purchase-form.html", {"request": request})
