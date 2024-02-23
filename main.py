aec84aed25a7271b160fae647c3833acdf1ce429
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-23 21:20:14 +0800

    make basic purchase tracking form

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+""" Main application file """
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
