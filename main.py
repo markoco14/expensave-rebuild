27caf7ba89d86775993370e71ec0aea0f1b61eaa
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-24 11:19:23 +0800

    connect signup form to POST route and redirect to home page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """ Main application file """
 from typing import Annotated
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Response
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -24,6 +24,27 @@ def get_sign_up_page(request: Request):
         request=request,
         name="signup.html",
     )
+
+@app.post("/signup", response_class=Response)
+def signup(
+    request: Request,
+    response: Response,
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    # db: Annotated[Session, Depends(get_db)],
+    ):
+    """Sign up a user"""
+    response = Response(status_code=200)
+    # response.set_cookie(
+    #     key="session-id",
+    #     value=session_cookie,
+    #     httponly=True,
+    #     secure=True,
+    #     samesite="Lax"
+    # )
+    response.headers["HX-Redirect"] = "/"
+
+    return response
 
 @app.post("/track-purchase")
 def track_purchase(
