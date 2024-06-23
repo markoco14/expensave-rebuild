53184cdd15f81135b709d2d7f55b583b6c0bf2e8
Mark O'Connor <mark.oconnor14@gmail.com>
2024-06-23 12:19:32 +0800

    add sleep time to config

--- app/core/config.py
+++ app/core/config.py
@@ -19,6 +19,7 @@ class Settings(BaseSettings):
 
 
     if ENVIRONMENT == 'dev':
+        SLEEP_TIME: int = 1
         DB_USER: str = os.environ.get('DEV_DB_USER')
         DB_PASSWORD: str = os.environ.get('DEV_DB_PASSWORD')
         DB_HOST: str = os.environ.get('DEV_DB_HOST')
