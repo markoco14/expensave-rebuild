5e05b30f4695124c290df1981bd9d534ff76791d
Mark <mark.oconnor14@gmail.com>
2024-11-26 10:20:25 +0800

    set up device list using enumerateDevices

--- static/js/camera.js
+++ static/js/camera.js
@@ -1,1 +1,29 @@
-console.log("camera.js loaded");
+`
+Contains all camera related functions
+`
+
+const devicesButton = document.getElementById("devicesButton");
+window.addEventListener("load", listAvailableDevices);
+
+if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}})
+}
+
+async function listAvailableDevices() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+        console.log("enumerateDevices() not supported.");
+        return;
+    } 
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    if (!devices) {
+        devicesList.innerHTML = "Unable to load device list. Try restarting the page.";
+        return
+    }
+    setTimeout(() => {
+        devicesList.innerHTML = "";
+        devices.forEach(device => {
+            const deviceItem = `<li>Device: ${device.kind}</li>`;
+            devicesList.innerHTML += deviceItem;
+        });
+    }, 1000)
+}
