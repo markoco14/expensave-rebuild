084048e125fdb3f2f78ac0fec236d6eb7c05c4a0
Mark <mark.oconnor14@gmail.com>
2024-12-11 10:19:30 +0800

    add image display after taking photo

--- static/js/camera.js
+++ static/js/camera.js
@@ -1,6 +1,14 @@
 const cameraButton = document.getElementById('cameraButton');
 const cameraInput = document.getElementById('cameraInput');
+const cameraImage = document.getElementById('cameraImage');
 
 cameraButton.addEventListener('click', () => {
   cameraInput.click();
+});
+
+cameraInput.addEventListener('change', () => {
+  const file = cameraInput.files[0];
+  const url = URL.createObjectURL(file);
+  cameraImage.src = url;
+  cameraImage.classList.toggle('hidden');
 });
