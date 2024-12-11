1fc679026515ec5b2b73100e3a1bdbdccb5308bb
Mark <mark.oconnor14@gmail.com>
2024-12-11 09:57:24 +0800

    add basic file input with capture to access device camera

--- static/js/camera.js
+++ static/js/camera.js
@@ -27,3 +27,27 @@ async function listAvailableDevices() {
         });
     }, 1000)
 }
+
+const video = document.querySelector('video');
+const canvas = document.querySelector('canvas');
+const screenshotImage = document.querySelector('img');
+const buttons = [...document.querySelectorAll('.js-controls')];
+let streamStarted = false;
+
+// isolate buttons
+const [play, pause, screenshot] = buttons;
+
+const constraints = {
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440
+      },
+    }
+  };
