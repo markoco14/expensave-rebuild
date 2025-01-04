9bbc257cd5cd4371fe94b23fb2bfafb15e96bb0c
Mark <mark.oconnor14@gmail.com>
2025-01-04 12:03:02 +0800

    implement store uploaded photo in s3 and object key in database

--- app/camera/camera_router.py
+++ app/camera/camera_router.py
@@ -1,8 +1,10 @@
 """Camera feature routes"""
 from io import BytesIO
 import os
+import time
 from typing import Annotated
 
+import boto3
 from fastapi import APIRouter, Depends, File, Request, UploadFile
 from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -13,7 +15,7 @@ from PIL import Image
 from app.auth import auth_service
 from app.core.database import get_db
 from app.core import links
-import time
+from app.transaction.transaction_model import Transaction
 
 
 router = APIRouter()
@@ -75,18 +77,43 @@ def upload_photo(
         response = RedirectResponse(url="/", status_code=303)
         return response
     
-    upload_dir = "./temp/images/raw/v1"
-    os.makedirs(upload_dir, exist_ok=True)
-    image = Image.open(BytesIO(photo.file.read()))
-    image = image.rotate(270)
+    # upload to s3
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_DEFAULT_REGION")
+    )
     upload_time = round(time.time() * 1000)
+    image_key = f"images/original/{current_user.id}-{upload_time}.png"
     try:
-        image.save(f"{upload_dir}/{current_user.id}-{upload_time}.png")
-    except:
+        s3.put_object(
+            Bucket=os.environ.get("AWS_PROJECT_BUCKET"),
+            Key=image_key,
+            Body=photo.file.read()
+        )
+    except Exception as e:
         response = JSONResponse(
                 status_code=303,
                 content={"message": "Camera upload failed!"}
             )
+        response.headers["hx-trigger"] = "cameraUploadFailed"
+        return response
+    
+    # save the image in the database
+    dbTransaction = Transaction(
+        user_id=current_user.id,
+        s3_key=image_key
+    )
+    try:
+        db.add(dbTransaction)
+        db.commit()
+        db.refresh(dbTransaction)
+    except Exception as e:
+        response = JSONResponse(
+                status_code=303,
+                content={"message": "Failed to upload photo!"}
+        )
         response.headers["hx-trigger"] = "cameraUploadFailed"
         return response
     
