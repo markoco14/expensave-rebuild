25913e88926eff76d9a961bc855031dfbf1ab879
Mark <mark.oconnor14@gmail.com>
2025-02-02 14:54:18 +0800

    update transaction schemas for store with less details and update inputs for form values

--- app/transaction/transaction_schemas.py
+++ app/transaction/transaction_schemas.py
@@ -4,7 +4,12 @@ from decimal import Decimal
 from pydantic import BaseModel
 
 
-
+class PurchaseCreateMinimal(BaseModel):
+	""" A data type for creating a purchase """
+	user_id: int
+	price: Decimal
+	receipt_lottery_number: str
+	purchase_time: datetime
 
 class PurchaseCreate(BaseModel):
 	""" A data type for creating a purchase """
