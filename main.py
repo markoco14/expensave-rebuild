b4863cdda2cb950748890b2eeaa116b0a12cec7f
Mark <mark.oconnor14@gmail.com>
2025-02-05 10:50:07 +0800

    change app-home.html to index.html

--- main.py
+++ main.py
@@ -79,40 +79,6 @@ def get_index_page(
     
     return RedirectResponse(url="/date/{}".format(datetime.now().strftime("%Y-%m-%d")))
 
-    purchases = transaction_service.get_user_today_purchases(
-        current_user_id=current_user.id, db=db)
-    
-    for purchase in purchases:
-        purchase.purchase_time = time_service.format_taiwan_time(purchase_time=purchase.purchase_time)
-
-
-    # user_timezone = "Asia/Taipei"  # replace with user's timezone one day
-    # purchases = time_service.adjust_purchase_dates_for_local_time(
-    #     purchases=purchases, user_timezone=user_timezone)
-
-    totalSpent = transaction_service.calculate_day_total_spent(
-        purchases=purchases)
-
-    currency = "TWD"
-    user_data = {
-        "display_name": current_user.display_name,
-        "is_admin": current_user.is_admin,
-    }
-
-    today_date = datetime.now()
-
-    context = {
-        "user": current_user,
-        "request": request,
-        "today_date": today_date,
-        "currency": currency,
-        "purchases": purchases,
-        "totalSpent": totalSpent,
-    }
-    return templates.TemplateResponse(
-        name="app/app-home.html",
-        context=context
-    )
 
 @app.get("/date/{selected_date}", response_class=templates.TemplateResponse)
 def get_app_index_page(
@@ -163,7 +129,7 @@ def get_app_index_page(
     }
 
     return templates.TemplateResponse(
-        name="app/app-home.html",
+        name="app/index.html",
         context=context
     )
 
