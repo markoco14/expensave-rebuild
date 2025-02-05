76c195d6ced1d9650119cd1b61e8ba0d22920659
Mark <mark.oconnor14@gmail.com>
2025-02-05 12:58:35 +0800

    split lottery id into letter input and number input
    to improve the user experience

--- main.py
+++ main.py
@@ -138,7 +138,8 @@ def get_app_index_page(
 def store_new_purchase(
     request: Request,
     selected_date: datetime,
-    lottery: Annotated[str, Form(...)],
+    lottery_letters: Annotated[str, Form(...)],
+    lottery_numbers: Annotated[str, Form(...)],
     time: Annotated[str, Form(...)],
     amount: Annotated[str, Form(...)],
     db: Session = Depends(get_db),
@@ -157,32 +158,49 @@ def store_new_purchase(
         )
     
     form_values = {
-        "lottery": lottery,
+        "lottery_letters": lottery_letters,
+        "lottery_numbers": lottery_numbers,
         "time": time,
         "amount": amount
     }
 
     form_errors = {}
 
-    # validate lottery number
-    if not lottery:
-        form_errors["lottery"] = "Please enter a lottery number."
+    # validate lottery info
+    if not lottery_letters and not lottery_numbers:
+        form_errors["lottery"] = "Lottery ID letters and numbers are required."
+    elif not lottery_letters:
+        form_errors["lottery"] = "Lottery ID numbers are required."
+    elif not lottery_numbers:
+        form_errors["lottery"] = "Lottery ID letters are required."
+    elif not lottery_letters.isalpha():
+        form_errors["lottery"] = "Lottery ID letters must be A-Z."
+    elif not lottery_letters.isupper():
+        form_errors["lottery"] = "Lottery ID letters must be uppercase."
+    elif len(lottery_letters) != 2:
+        form_errors["lottery"] = "There can only be 2 letters."
+    elif not lottery_numbers.isdigit():
+        form_errors["lottery"] = "Lottery ID numbers must be 0-9."
+    elif len(lottery_numbers) < 8:
+        form_errors["lottery"] = "Lottery number must have at least 8 numbers"
+    elif len(lottery_numbers) > 8:
+        form_errors["lottery"] = "Lottery number must have at most 8 numbers"
     
     # first check whether the lottery number has at least 8 digits
-    digit_regex = r"(\d+)"
-    lottery = lottery.upper()
-    digit_match = re.search(digit_regex, lottery)
+    # digit_regex = r"(\d+)"
+    # lottery_numbers = lottery_numbers.upper()
+    # digit_match = re.search(digit_regex, lottery_numbers)
 
-    try:
-        lottery_digits = digit_match.group()
-    except AttributeError:
-        lottery_digits = None
+    # try:
+    #     lottery_digits = digit_match.group()
+    # except AttributeError:
+    #     lottery_digits = None
 
-    # validate lotttery number is at least 8 digits
-    if not lottery_digits:
-        form_errors["lottery"] = "Please enter a valid lottery number."
-    elif len(lottery_digits) != 8:
-        form_errors["lottery"] = "Lottery number must be 8 digits digits."
+    # # validate lotttery number is at least 8 digits
+    # if not lottery_digits:
+    #     form_errors["lottery"] = "Please enter a valid lottery number."
+    # elif len(lottery_digits) != 8:
+    #     form_errors["lottery"] = "Lottery number must be 8 digits digits."
 
     # in case somehow not a time
     if not time:
@@ -220,11 +238,16 @@ def store_new_purchase(
         return response
     
     utc_corrected_time = datetime.combine(selected_date, purchase_time_object) - timedelta(hours=8)
+
+    if lottery_letters:
+        final_lottery_number = f"{lottery_letters}-{lottery_numbers}"
+    else:
+        final_lottery_number = lottery_numbers
     
     new_purchase = transaction_schemas.PurchaseCreateMinimal(
         user_id=current_user.id,
         price=amount,
-        receipt_lottery_number=lottery,
+        receipt_lottery_number=final_lottery_number,
         purchase_time=utc_corrected_time
     )
 
