86488807410837a9f9b9587d8fdbebc3000f7c18
Mark O'Connor <mark.oconnor14@gmail.com>
2026-01-25 11:23:49 +0800

    create sql files for tables

---
+++ app/sql/tables/bucket.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bucket(
+            bucket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            amount INTEGER NOT NULL,
+            is_daily INTEGER NOT NULL DEFAULT 0,
+            month_start TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE
+        );
+CREATE UNIQUE INDEX idx_bucket_name_month_user
+        ON bucket(user_id, month_start, name);
---
+++ app/sql/tables/purchase.sql
@@ -0,0 +1,14 @@
+CREATE TABLE purchase(
+            purchase_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount INTEGER NOT NULL,
+            currency TEXT NOT NULL,
+            lotto_number TEXT,
+            purchased_at TEXT NOT NULL,
+            timezone TEXT NOT NULL,
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            updated_at TEXT,
+            user_id INTEGER NOT NULL,
+            bucket_id INTEGER NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+            FOREIGN KEY(bucket_id) REFERENCES bucket(bucket_id) ON DELETE CASCADE
+        );
