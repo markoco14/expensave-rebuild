8a617a8f5220c7eb08430e091f92319daef21ede
Mark O'Connor <mark.oconnor14@gmail.com>
2026-01-26 23:51:22 +0800

    create bucket class

---
+++ app/models/bucket.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+import sqlite3
+
+@dataclass
+class Bucket:
+    bucket_id: int
+    name: str = None
+    amount: int = None
+    is_daily: bool = None
+    month_start: date = None
+    created_at: datetime = None
+    updated_at: datetime = None
+    user_id: int = None
+
+    @classmethod
+    def list_for_month(cls, month_start: str, user_id: int):
+        with sqlite3.connect("db.sqlite3") as conn:
+            conn.execute("PRAGMA foreign_keys = ON;")
+            conn.row_factory = sqlite3.Row
+
+            cursor = conn.cursor()
+            cursor.execute("SELECT bucket_id, name, amount, is_daily FROM bucket WHERE month_start = ? AND user_id = ?;", (month_start, user_id))
+            return [Bucket(**row) for row in cursor.fetchall()]
