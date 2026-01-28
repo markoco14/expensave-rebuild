3fedf3e21f1572b42d590481b9f61d9750d1c0ad
Mark O'Connor <mark.oconnor14@gmail.com>
2026-01-28 10:49:44 +0800

    create purchase.show route and controller method

---
+++ app/controllers/hv/purchase.py
@@ -0,0 +1,44 @@
+import asyncio
+from calendar import monthrange
+from datetime import date, datetime, timedelta, timezone
+import sqlite3
+import time
+from types import SimpleNamespace
+import uuid
+from zoneinfo import ZoneInfo
+
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+from app import cryptography
+from app.models.bucket import Bucket
+
+
+templates = Jinja2Templates(directory="templates")
+
+async def show(request: Request, purchase_id: int):
+    accept_header = request.headers.get("accept", "")
+    content_type = "application/vnd.hyperview+xml" if "hyperview" in accept_header else "text/xml"
+
+    with sqlite3.connect("db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("""
+                        SELECT purchase.purchase_id,
+                        purchase.amount,
+                        purchase.currency,
+                        purchase.purchased_at,
+                        purchase.timezone,
+                        bucket.name as bucket_name 
+                        FROM purchase JOIN bucket USING (bucket_id) 
+                        WHERE purchase_id = ?;
+                       """,
+                       (purchase_id, ))
+        purchase = cursor.fetchone()
+
+    return templates.TemplateResponse(
+        request=request,
+        name="hv/purchases/show.xml",
+        context={"purchase": purchase},
+        headers={"Content-Type": content_type}
+    )
