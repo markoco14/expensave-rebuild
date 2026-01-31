17bda2a2e62d4901da65cb7830752cc704d85163
Mark O'Connor <mark.oconnor14@gmail.com>
2026-01-31 23:07:28 +0800

    update schemas

--- app/sql/tables/bucket.sql
+++ app/sql/tables/bucket.sql
@@ -1,13 +1,8 @@
-CREATE TABLE bucket(
+CREATE TABLE IF NOT EXISTS "bucket"(
             bucket_id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            amount INTEGER NOT NULL,
-            is_daily INTEGER NOT NULL DEFAULT 0,
-            month_start TEXT NOT NULL,
+            is_daily INTEGER NOT NULL CHECK (is_daily IN (0, 1)) DEFAULT 0,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME,
             user_id INTEGER NOT NULL,
             FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE
-        );
-CREATE UNIQUE INDEX idx_bucket_name_month_user
-        ON bucket(user_id, month_start, name);
+            );
