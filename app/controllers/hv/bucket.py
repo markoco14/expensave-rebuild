a503250df1e660c5a2f97581b043c54acc0e48c2
Mark O'Connor <mark.oconnor14@gmail.com>
2026-04-19 13:56:41 +0800

    add logged spending query to bucket show page

--- app/controllers/hv/bucket.py
+++ app/controllers/hv/bucket.py
@@ -1,8 +1,6 @@
 import calendar
 from datetime import datetime, timezone
-from pprint import pprint
 import sqlite3
-from types import SimpleNamespace
 from zoneinfo import ZoneInfo
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
@@ -10,7 +8,7 @@ from fastapi.templating import Jinja2Templates
 from app.models.bucket import Bucket
 from app.models.bucket_month_top_up import BucketMonthTopUp
 from app.respository.bucket import get_with_top_up, list_with_top_ups
-from app.respository.purchase import list_for_bucket_and_month
+from app.respository import purchase as purchase_repo
 
 
 templates = Jinja2Templates(directory="templates")
@@ -85,7 +83,7 @@ async def show(request: Request, bucket_id: int):
             conn.execute("PRAGMA foreign_keys = ON;")
             conn.row_factory = sqlite3.Row
 
-            purchase_rows = list_for_bucket_and_month(
+            purchase_rows = purchase_repo.list_for_bucket_and_month(
                 conn=conn,
                 bucket_id=bucket_id,
                 utc_month_start=utc_month_start,
@@ -105,6 +103,14 @@ async def show(request: Request, bucket_id: int):
         conn.row_factory = sqlite3.Row
 
         bucket_top_up_join = get_with_top_up(conn=conn, month_start=month_start, bucket_id=bucket_id)
+
+        logged_spending = purchase_repo.get_logged_spend_for_bucket_month(
+            conn=conn,
+            bucket_id=bucket_id,
+            utc_month_start=utc_month_start,
+            utc_month_end=utc_month_end
+            )
+
 
     if not bucket_top_up_join:
         return templates.TemplateResponse(
@@ -142,6 +148,7 @@ async def show(request: Request, bucket_id: int):
         context={
             "bucket": bucket,
             "top_up": top_up,
-            "amount_spent": amount_spent
+            "amount_spent": amount_spent,
+            "logged_spending": int(logged_spending)
             }
     )
